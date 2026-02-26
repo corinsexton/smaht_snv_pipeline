bf82d5c57f898e4c94f5144a4f360303bd752fe0
Corinne Sexton <corinsexton@gmail.com>
2026-02-26 12:23:01 -0500

    .

--- run_snv_pipeline.sh
+++ run_snv_pipeline.sh
@@ -8,13 +8,13 @@
 #SBATCH -c 1
 #SBATCH -t 36:00:00
 #SBATCH -o slurm-%x.%j.out
-#SBATCH --dependency=afterany:25861666
+##SBATCH --dependency=afterany:25861666
 
-nextflow run main.nf  \
+nextflow run main.nf -resume  \
   --vep_config vep.ini \
   --longread_csv Production_lr.csv \
   --ont_csv Production_ont.csv \
   --shortread_csv Production_sr.csv \
   --input_metadata Production_metadata.csv \
-  --input_vcfs Production_vcfs.csv \
-  --results_dir ./Production_results
+  --input_vcfs Production_vcfs_ss.csv \
+  --results_dir ./results
